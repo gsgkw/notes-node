b78047cd9e7a7985edffaa19c5617035b8e4d813
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-16 23:54:08 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('starting app.');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js')
+
+var res = notes.addNote();
+console.log(res);
+
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
+//   if (err) {
+//     console.log('unable to write to file');
+//   }
+// });
