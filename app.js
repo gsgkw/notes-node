5a64e0ffd344f8888458f7f8a3bd09c83de8b612
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-17 12:03:35 +0000

    adds new module to export

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ console.log('starting app.');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js')
+
+var res = notes.delNote();
+console.log(res);
+
+// var user = os.userInfo();
 //
-// var res = notes.addNote();
-// console.log(res);
-
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
-  if (err) {
-    console.log('unable to write to file');
-  }
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
+//   if (err) {
+//     console.log('unable to write to file');
+//   }
+// });
--- notes.js
+++ notes.js
@@ -4,3 +4,9 @@ module.exports.addNote = () => {
   console.log('addNote');
   return 'new Note';
 };
+
+
+module.exports.delNote = () => {
+  console.log('delete note');
+  return 'deleted Note';
+};
